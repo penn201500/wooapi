97866d010ba11060f97eef9881ffd23be04765db
penn <penn201500@gmail.com>
2024-03-04 22:52:17 +0800

    1. update README.md; 2. Format the output content in right panel

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Put the HMAC signature in request header x-api-signature, and put timestamp in x
 
 If you use Python, this is an example to get signature:
 
-``` python
+```python
 import datetime
 import hmac, hashlib 
 import requests
@@ -77,6 +77,54 @@ if __name__ == '__main__':
 ```
 
 ### WebSocket
+
+Read: https://vipdocs.woo.org/?javascript#websocket-api-v2
+
+Here is an example:
+
+```javascript
+import WebSocket from 'ws';
+
+const API_SERVER_WSS = 'wss://wss.woo.org/ws/stream';
+const applicationId = process.env.WOO_API_APP_ID;
+const wssUrl = `${API_SERVER_WSS}/${applicationId}`;
+const socket = new WebSocket(wssUrl);
+
+console.log('WebSocket URL:', wssUrl);
+
+socket.onopen = function(event) {
+    console.log('WebSocket is connected.');
+
+    // The server will send a ping command to the client every 10 seconds.
+    // If the pong from client is not received within 10 seconds for 10 consecutive times,
+    // it will actively disconnect the client.
+    const pongMsg = JSON.stringify({ event: 'pong' });
+
+    // Send a pong message every 10 seconds to keep the connection alive
+    setInterval(() => {
+        socket.send(pongMsg);
+    }, 10000);
+};
+
+socket.onmessage = function(event) {
+    console.log('Message from server:', event.data);
+    const data = JSON.parse(event.data);
+
+    // If the server requires a pong message, respond accordingly
+    if (data && data.event === 'ping') {
+        socket.send(JSON.stringify({ event: 'pong' }));
+    }
+};
+
+socket.onerror = function(error) {
+    console.error('WebSocket Error:', error);
+};
+
+socket.onclose = function(event) {
+    console.log('WebSocket is closed now.');
+};
+
+```
 
 ## Deployment
 
--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const API_SERVER = 'http://localhost:63343';
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|true|false|null|-?\d+(\.\d*)?([eE][+\-]?\d+)?)/g, function (match) {
+        let cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('change', function () {
@@ -17,8 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(data => {
                 console.log(data)
                 const outputDiv = document.querySelector('.right-panel .output');
-                // Assuming the response is JSON and you want to display it as text
-                outputDiv.textContent = JSON.stringify(data, null, 2);
+                // Format and highlight the JSON
+                const formattedData = syntaxHighlight(data);
+                outputDiv.innerHTML = `<pre>${formattedData}</pre>`;
+                // outputDiv.textContent = JSON.stringify(data, null, 2);
             })
             .catch(error => {
                 console.error('Error fetching account info:', error);
@@ -28,30 +53,32 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 // Click event for "Get Available Symbols" sub-item
-        document.getElementById('getAvailableSymbols').addEventListener('click', function (event) {
-            event.stopPropagation(); // Prevent triggering click event on the parent item
-            const outputDiv = document.querySelector('.right-panel .output');
-            outputDiv.textContent = 'Fetching data, please wait...';
-            fetch(`${API_SERVER}/getAvailableSymbols`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    const outputDiv = document.querySelector('.right-panel .output');
-                    // Assuming the response is JSON and you want to display it as text
-                    outputDiv.textContent = JSON.stringify(data, null, 2);
-                })
-                .catch(error => {
-                    console.error('Error fetching account info:', error);
-                    const outputDiv = document.querySelector('.right-panel .output');
-                    outputDiv.textContent = 'Error fetching data: ' + error;
-                });
-        });
-
-        const headers = document.querySelectorAll('.item-header');
-        headers.forEach(header => {
-            header.addEventListener('click', () => {
-                const subItems = header.nextElementSibling;
-                subItems.classList.toggle('show');
+    document.getElementById('getAvailableSymbols').addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent triggering click event on the parent item
+        const outputDiv = document.querySelector('.right-panel .output');
+        outputDiv.textContent = 'Fetching data, please wait...';
+        fetch(`${API_SERVER}/getAvailableSymbols`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                const outputDiv = document.querySelector('.right-panel .output');
+                // Format and highlight the JSON
+                const formattedData = syntaxHighlight(data);
+                outputDiv.innerHTML = `<pre>${formattedData}</pre>`;
+                // outputDiv.textContent = JSON.stringify(data, null, 2);
+            })
+            .catch(error => {
+                console.error('Error fetching account info:', error);
+                const outputDiv = document.querySelector('.right-panel .output');
+                outputDiv.textContent = 'Error fetching data: ' + error;
             });
+    });
+
+    const headers = document.querySelectorAll('.item-header');
+    headers.forEach(header => {
+        header.addEventListener('click', () => {
+            const subItems = header.nextElementSibling;
+            subItems.classList.toggle('show');
         });
+    });
 })
